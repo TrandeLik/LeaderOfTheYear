aaa0d0ff63c539f64f91c866465ea1fe0e56902c
VOODOO <kubamba2002@gmail.com>
2019-05-31 19:10:01 +0300

    Migration for achievement types

---
+++ database/migrations/2019_05_31_155500_create_achievement_types_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAchievementTypesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('achievement_types', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('type');
+            $table->string('stage');
+            $table->string('result');
+            $table->integer('score');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('achievement_types');
+    }
+}
