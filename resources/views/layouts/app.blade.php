905726e23e84f6bece16ce641c40ae88d73b3938
VOODOO <kubamba2002@gmail.com>
2019-06-14 23:02:10 +0300

    Updated nav panel

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -55,13 +55,14 @@
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="/">Главная</a>
+                                    <a class="dropdown-item" href="/profile">Профиль</a>
+                                    <a class="dropdown-item" href="/leaderboard">Рейтинговая таблица</a>
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
                                         {{ __('Выход') }}
                                     </a>
-                                    <a class="dropdown-item" href="/profile">Профиль</a>
-                                    <a class="dropdown-item" href="/leaderboard">Рейтинговая таблица</a>
                                     <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                                         @csrf
                                     </form>
