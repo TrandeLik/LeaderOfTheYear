847bd48e7b32b59841ea0a5c95c8d10042cf2d5b
VOODOO <kubamba2002@gmail.com>
2019-07-16 16:51:20 +0300

    Added personal agreement

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -55,6 +55,7 @@ class RegisterController extends Controller
             'role' => 'required',
             'form' => 'required',
             'formLetter' => 'required',
+            'personal_agreement' => 'required',
         ]);
     }
 
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -107,6 +107,39 @@
                             </div>
                         </div>
 
+                        <div class="row align-items-center form-group col-12 text-center">
+                            <div class="col-md-6">
+                                <input id="personal-agreement" type="checkbox" name="personal_agreement" required>
+                            </div>
+                            <div class="col-md-6 ofset-0">
+                                <p style="margin-left: 0px;">Я принимаю условия
+                                <a data-toggle="modal" data-target="#exampleModalLong" style="color:blue;">
+                                    пользовательского соглашения
+                                </a>
+                                </p>
+                            </div>
+                            <!-- Modal -->
+                            <div class="modal fade" id="exampleModalLong" tabindex="-1" role="dialog" aria-labelledby="exampleModalLongTitle" aria-hidden="true">
+                            <div class="modal-dialog" role="document">
+                                <div class="modal-content">
+                                <div class="modal-header">
+                                    <h5 class="modal-title" id="exampleModalLongTitle">Modal title</h5>
+                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                    </button>
+                                </div>
+                                <div class="modal-body">
+                                    ...
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                </div>
+                                </div>
+                            </div>
+                            </div>
+                            
+                        </div>
+
                         <input type="hidden" name="role" value="student">
 
                         <div class="form-group row mb-0">
