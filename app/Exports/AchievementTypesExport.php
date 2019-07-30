f1db32eccffc35294e9209a204f90a6a2d3c1264
VOODOO <kubamba2002@gmail.com>
2019-07-30 16:08:43 +0300

    Fixed achievement types export

--- app/Exports/AchievementTypesExport.php
+++ app/Exports/AchievementTypesExport.php
@@ -3,31 +3,28 @@
 namespace App\Exports;
 
 use App\AchievementType;
-use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\FromQuery;
 use Maatwebsite\Excel\Concerns\WithHeadings;
 use Maatwebsite\Excel\Concerns\ShouldAutoSize;
 
-class AchievementTypesExport implements FromCollection, WithHeadings, ShouldAutoSize
+class AchievementTypesExport implements FromQuery, WithHeadings, ShouldAutoSize
 {
     /**
     * @return \Illuminate\Support\Collection
     */
-    public function collection()
+    public function query()
     {
-        return AchievementType::all();
+        return AchievementType::select('category','type','stage','result','score');
     }
 
     public function headings(): array
     {
         return [
-            '#',
             'Категория',
             'Тип',
             'Этап',
             'Результат',
             'Баллы',
-            'created_at',
-            'updated_at',
         ];
     }
 
